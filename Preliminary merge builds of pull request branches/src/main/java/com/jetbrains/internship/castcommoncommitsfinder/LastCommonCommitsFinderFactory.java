ae7f4ca270fa224df83370d0806958914918bc70
Roman <b111forjob@gmail.com>
2021-04-10 13:10:58 +0700

    Copy interfaces from assignement

---
+++ Preliminary merge builds of pull request branches/src/main/java/com/jetbrains/internship/castcommoncommitsfinder/LastCommonCommitsFinderFactory.java
@@ -0,0 +1,17 @@
+package com.jetbrains.internship.castcommoncommitsfinder;
+
+public interface LastCommonCommitsFinderFactory {
+
+    /**
+     * Creates an instance of LastCommonCommitsFinder for a particular GitHub.com repository.
+     * This method must not check connectivity.
+     *
+     * @param owner repository owner
+     * @param repo  repository name
+     * @param token personal access token or null for anonymous access
+     * @return an instance of LastCommonCommitsFinder
+     */
+    LastCommonCommitsFinder create(String owner, String repo, String token);
+
+}
+
